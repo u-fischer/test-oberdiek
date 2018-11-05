594b420feb0b675b650080aa4fbfa67188da6013
Ulrike Fischer <fischer@troubleshooting-tex.de>
2018-11-05 23:14:43 +0100

    more tests

--- source/ExampleUTF8.java
+++
@@ -1,8 +0,0 @@
-public class ExampleUTF8 {
-    public static String testString =
-        "Umlauts: " +
-        "ÃÃÃÃ¤Ã¶Ã¼Ã";
-    public static void main(String[] args) {
-        System.out.println(testString);
-    }
-}
